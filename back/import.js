580b28f48a53f210ad883399726f913ec9981c75
acalcagno <calqui@gmail.com>
2015-04-15 01:34:11 -0300

    preparando cte control

--- back/import.js
+++ back/import.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/concursantes', function(err, res) {
+mongoose.connect('mongodb://localhost/perfiles', function(err, res) {
     if(err) {
         console.log('ERROR: conectando la db. ' + err);
     } else {
@@ -14,26 +14,24 @@ lineList.shift(); // Shift the headings off the list of records.
 
 var schemaKeyList = ['dni', 'apellido', 'nombre', 'perfil' ,'ponderacion'];
 
-var Concursantes = new mongoose.Schema({
-    perfiles: [{
+var PerfilSchema = new mongoose.Schema({
+    nombre: String,
+    documentacionRequerida: [String],
+    postulantes: [{
+        apellido: String,
         nombre: String,
-        documentacionRequerida:[String],
-        postulantes:[{
-            apellido: String,
-            nombre: Sting,
-            ponderacion: Number,
-            documento: Number,
-            documentosPresentados: [{
-                documento:String,
-                cantidadFojas:Number}],
+        dni: String,
+        ponderacion: Number,
+        documentacionPresentada: [{
+            documento: String,
+            cantidadFojas: Number
         }]
     }]
 });
-
-var Concursantes = mongoose.model('Concursantes', Concursantes);
+var Perfil = mongoose.model('perfil', PerfilSchema);
 
 function queryAllEntries () {
-    Concursantes.aggregate(
+    /*Concursantes.aggregate(
         {$group: {_id: '$RepName', oppArray: {$push: {
             OppID: '$OppID', 
             OppName: '$OppName',
@@ -43,9 +41,23 @@ function queryAllEntries () {
         }}, function(err, qDocList) {
         console.log(util.inspect(qDocList, false, 10));
         process.exit(0);
-    });
+    });*/
 }
 
+
+function postulanteFrom(row) {
+    var postulante = {}
+    postulante.apellido = row[1];
+    postulante.nombre = row[2];
+    postulante.dni = row[0];
+    postulante.ponderacion = row[4];
+    console.log('crea el postulante ' + postulante);
+    return postulante;
+}
+
+var nombre_perfil_row_anterior = "NINGUNO";
+var postulantes_del_perfil = [];
+var perfil = new Perfil();
 // Recursively go through list adding documents.
 // (This will overload the stack when lots of entries
 // are inserted.  In practice I make heavy use the NodeJS 
@@ -55,15 +67,43 @@ function createDocRecurse (err) {
         console.log(err);
         process.exit(1);
     }
+    console.log('registros restantes ', lineList.length)
     if (lineList.length) {
         var line = lineList.shift();
-        var doc = new RepOppDoc();
-        line.split(',').forEach(function (entry, i) {
-            doc[schemaKeyList[i]] = entry;
-        });
-        doc.save(createDocRecurse);
+        var row = line.split(',')
+
+        nombre_perfil_row = row[3];
+        
+        console.log('son iguales? ' + nombre_perfil_row_anterior + ' con ' + nombre_perfil_row);
+        
+        if (nombre_perfil_row_anterior == nombre_perfil_row) {
+            console.log('debug permanece: ' + row);
+            postulantes_del_perfil.concat(postulanteFrom(row));
+        } else {
+            console.log('debug cambia: ' + row);
+            perfil.nombre = nombre_perfil_row_anterior;
+            perfil.postulantes.push(postulantes_del_perfil);
+
+            console.log('guarda ' + perfil);
+
+            perfil = new Perfil();
+            nombre_perfil_row_anterior = nombre_perfil_row;
+            perfil.postulantes = [];
+            postulante_a_agregar = postulanteFrom(row);
+            console.log('agrega posulante ' +postulante_a_agregar.nombre);
+            perfil.postulantes.push(postulante_a_agregar);
+            console.log('y los post quedan ' + postulantes_del_perfil);
+            createDocRecurse();
+        }
+        
+        
+        
+        /*.forEach(function (entry, i) {
+            perfil[schemaKeyList[i]] = entry;
+            console.log(doc);
+        });*/
+        //perfil.save(createDocRecurse);
     } else {
-        // After the last entry query to show the result.
         queryAllEntries();
     }
 }
