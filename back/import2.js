3e44c1c31e4c6f4e5e5139f08eef75830f4575df
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-04-17 19:48:55 -0300

    2_carga_de_fojas: importacion de perfiles

--- back/import2.js
+++ back/import2.js
@@ -9,9 +9,36 @@ mongodb.MongoClient.connect(uri, function(err, db) {
   	if(err) throw err;
 	var col_perfiles = db.collection('perfiles');
 	col_perfiles.find({}).toArray(function(err, perfiles){
-		var stream = fs.createReadStream("concursantes_ejemplo.csv");
+		var stream_perfiles = fs.createReadStream("perfiles.csv");
 		csv
-			.fromStream(stream, {headers : true})
+			.fromStream(stream_perfiles, {headers : true})
+			.on("data", function(data){     
+				var perfil = _.findWhere(perfiles, {nombre: data.perfil});
+				if(!perfil) {
+					perfil = {
+						nombre: data.perfil,
+						postulantes: [],
+						documentacionRequerida:[]
+					};
+					perfiles.push(perfil);
+				}
+			
+				if(!_.contains(perfil.documentacionRequerida, data.nombre_documento)) {
+					perfil.documentacionRequerida.push(data.nombre_documento);
+				}
+				})
+			.on("end", function(){
+				console.log(JSON.stringify(perfiles));
+				_.forEach(perfiles, function(perfil){
+					col_perfiles.save(perfil, function(err){
+						if(err) throw err;
+					});
+				});
+			});	
+		
+		var stream_concursantes = fs.createReadStream("concursantes.csv");
+		csv
+			.fromStream(stream_concursantes, {headers : true})
 			.on("data", function(data){     
 				var perfil = _.findWhere(perfiles, {nombre: data.perfil});
 				if(!perfil) {
@@ -41,6 +68,6 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 						if(err) throw err;
 					});
 				});
-			});									
+			});	
 	});	
 });
