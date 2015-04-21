1209a39698f883ae9fb7995af8ff147b9f5929f0
acalcagno <calqui@gmail.com>
2015-04-20 22:31:09 -0300

    modificacion de la importacion

--- back/import_expedientes.js
+++ back/import_expedientes.js
@@ -8,75 +8,60 @@ var uri = 'mongodb://127.0.0.1/ExpedientesConcurso';
 mongodb.MongoClient.connect(uri, function(err, db) {  
   	if(err) throw err;
 	var col_expedientes = db.collection('expedientes');
-	col_expedientes.find({}).toArray(function(err, expedientes){
+	col_expedientes.find({}).toArray(function(err, expedientes_db){
 		var stream_expedientes = fs.createReadStream("expedientes_cabecera.csv");
 		csv
 			.fromStream(stream_expedientes, {headers : true})
-			.on("data", function(data){     
-				var expediente = _.findWhere(expedientes, {nombre: data.expediente});
-				if(!expediente) {
-					expediente = {
-						nombre: data.expediente,
+			.on("data", function(csv_exp_cabe_data){     
+				var expediente_db = _.findWhere(expedientes_db, {nombre: csv_exp_cabe_data.expediente});
+				if(!expediente_db) {
+					expediente_db = {
+						nombre: csv_exp_cabe_data.expediente,
 						documentosCabecera: []
                         
 					};
-					expedientes.push(expediente);
+					expedientes_db.push(expediente_db);
 				}
 			
-				if(!_.contains(expediente.documentosCabecera, data.documentoCabecera)) {
-					expediente.documentosCabecera.push({ nombre: data.documentoCabecera, cantidadFolios: data.cantidadFolios, orden:data.orden });
+				if(!_.contains(expediente_db.documentosCabecera, csv_exp_cabe_data.documentoCabecera)) {
+					expediente_db.documentosCabecera.push({ nombre: csv_exp_cabe_data.documentoCabecera, cantidadFolios: csv_exp_cabe_data.cantidadFolios, orden:csv_exp_cabe_data.orden });
 				}
 				})
 			.on("end", function(){
-				console.log(JSON.stringify(expedientes));
-				_.forEach(expedientes, function(expediente){
-					col_expedientes.save(expediente, function(err){
+				//console.log(JSON.stringify(expedientes_db));
+				_.forEach(expedientes_db, function(expediente_db){
+					col_expedientes.save(expediente_db, function(err){
 						if(err) throw err;
 					});
 				});
-			});	
-		
-        
-		var stream_concursantes = fs.createReadStream("concursantes_en_expediente.csv");
-		
-        csv
-			.fromStream(stream_concursantes, {headers : true})
-			.on("data", function(data){     
-				
-                var expediente = _.findWhere(expedientes, {nombre: data.expediente});
-				if(!expediente) {
-                    console.log("Error de datos, no se encontro el expediente " + data.expediente);
-					/*perfil = {
-						nombre: data.perfil,
-						postulantes: [],
-						documentacionRequerida:[]
-					};
-					perfiles.push(perfil);*/
-				}
-                if(!expediente.postulantes) {
-                    expediente.postulantes = [];
-                }
-                expediente.postulantes.push({dniPostulante: data.dniPostulante, perfil: data.perfil});
-				/*var postulante = _.findWhere(expediente.postulantes, {dni: data.dni});
-				if(!postulante) {
-					postulante = {
-						nombre: data.nombre,
-						apellido: data.apellido,
-						ponderacion: data.ponderacion,
-						dni: data.dni,
-						documentosPresentados:[]
-					}
-					perfil.postulantes.push(postulante);
-				}*/
-				})
-			.on("end", function(){
-				//console.log(JSON.stringify(perfiles));
-				_.forEach(expedientes, function(expediente){
-					col_expedientes.save(expediente, function(err){
-						if(err) throw err;
-					});
-				});
-			});	
-            
-	});	
+			})
+            	
+
+            var col_perfiles = db.collection('perfiles');
+            col_perfiles.find({}).toArray(function(err, perfiles_db){
+                var stream_postulantes_en_expedientes = fs.createReadStream("concursantes_en_expediente.csv");
+                csv
+                    .fromStream(stream_postulantes_en_expedientes, {headers : true})
+                    .on("data", function(csv_postu_en_exp_data){     
+                        var perfil_db = _.findWhere(perfiles_db, {nombre:csv_postu_en_exp_data.perfil});
+                        
+                        var postulacion_db = _.findWhere(perfil_db.postulantes, {dni:csv_postu_en_exp_data.dniPostulante});
+                        var expediente_db = _.findWhere(expedientes_db, {nombre:csv_postu_en_exp_data.expediente});
+                        postulacion_db.incluidoEnExpediente = expediente_db._id;
+                        console.log(JSON.stringify(expediente_db._id));
+                        
+                        col_perfiles.save(perfil_db, function(err) {
+                            if(err) throw err;
+                        })
+                    })
+                    .on("end", function() {
+                });
+                /*
+                var stream_postulantes_en_expedientes = fs.createReadStream("concursantes_en_expediente.csv");
+                csv
+                    .fromStream(stream_postulantes_en_expedientes, {headers : true})
+                    .on("data", function(csv_postu_en_exp_data){     
+                    */
+            });
+	});
 });
